90d18993c16dba3154dd144d504a544273d68d05
MAGIC <tianzhonghiroki82@gmail.com>
2025-10-24 17:51:25 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// tasks/createTask.js から Firestoreトリガー関数を読み込む
 const { onUpdateHandler } = require("./tasks/createTask");
+
+// tasks/pushHandler.js から HTTP関数を読み込む
 const { pushHandler } = require("./tasks/pushHandler");
 
+// Firestore更新トリガー関数
 exports.onUpdateFirestore = onUpdateHandler;
-exports.sendMessage = pushHandler;
+
+// HTTP関数（Push エンドポイント）
+exports.sendMessage = (req, res) => {
+  pushHandler(req.body)
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(500).send(err.message));
+};
